4d0e8c53b55822e3a59f62998a82fa377f6715df
KamalrajVelmurugan <kamalrajvelmurugan21@gmail.com>
2024-07-10 11:43:08 -0700

    Update Success

--- src/main/java/com/onesoft/APIGateway/ApiGatewayApplication.java
+++ src/main/java/com/onesoft/APIGateway/ApiGatewayApplication.java
@@ -1,10 +1,16 @@
 package com.onesoft.APIGateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@ComponentScan
+@EnableAutoConfiguration
+@Configuration
 @EnableEurekaServer
 public class ApiGatewayApplication {
 
